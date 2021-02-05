84f3891a9865090e31a35df4bc22494dc9ca255e
donchase <donchase@gmail.com>
2021-02-05 11:28:09 -0500

    Added assets and media

---
+++ src/containers/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Card from '../components/Card/Card';
+
+class List extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Movie List from List.js</h1>
+                <Card />
+            </div>
+        );
+    }
+};
+
+export default List;
