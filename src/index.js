1bd4940e13c3e9c2325aacc765ed7f8cccb2f604
donchase <donchase@gmail.com>
2021-02-05 10:02:39 -0500

    Setting up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("movieList");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>movieList-Ubuntu</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
